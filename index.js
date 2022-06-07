167cabfe9eb2c97a084d3eea443bcb3a5157d02f
zzx <19957296561@163.com>
2022-06-07 15:43:15 +0800

    测试代码

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+ * @Author       : zzx 19957296561@163.com
+ * @Date         : 2022-06-07 15:42:39
+ * @LastEditors  : zzx 19957296561@163.com
+ * @LastEditTime : 2022-06-07 15:42:40
+ * @FilePath     : \npm\bai-utils\index.js
+ * @Description  : 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+export default function test(){
+    console.log('测试代码')
+}
